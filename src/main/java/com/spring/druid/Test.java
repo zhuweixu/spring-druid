777121ab69c01fd52a37143c85bdd681791d554a
朱 <1742116990@qq.com>
2023-01-13 10:25:53 +0800

    Test.java

---
+++ src/main/java/com/spring/druid/Test.java
@@ -0,0 +1,10 @@
+package com.spring.druid;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s1 = "test1";
+        String s2 = "test2";
+        String s3 = "test3";
+    }
+}
